063787a6d48543e8bf8d6cca76acaa47d0f7b16d
marcin kossakowski <morianekxd@gmail.com>
2025-01-15 23:20:54 +0100

    added register site and added basic ass form

--- auth/views.py
+++ auth/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def login(request):
+    if request.method == 'POST':
+        print(request.POST)
+        return HttpResponse('Login')
     return render(request, 'auth/login.html')
+
+def register(request):
+    if request.method == 'POST':
+        print(request.POST)
+        return HttpResponse('Register')
+    return render(request, 'auth/register.html')
