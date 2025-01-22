92a44c5cb11603da97d1f1ad2427c77837e267c0
marcin kossakowski <morianekxd@gmail.com>
2025-01-22 22:20:21 +0100

    added logout

--- authentication/views.py
+++ authentication/views.py
@@ -1,15 +1,19 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
 # Create your views here.
 def login(request):
     if request.method == 'POST':
         print(request.POST)
-        return HttpResponse('Login')
+        return redirect('main')
     return render(request, 'authentication/login.html')
 
 def register(request):
     if request.method == 'POST':
         print(request.POST)
-        return HttpResponse('Register')
+        return redirect('main')
     return render(request, 'authentication/register.html')
+
+def log_out(request):
+    logout(request)
+    return redirect('main')
