c7b48f7613e09096e8de65011994c7d09056e07a
marcin kossakowski <morianekxd@gmail.com>
2025-01-22 22:13:24 +0100

    added custom user to django admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'is_active', 'is_staff', 'date_joined')
+    search_fields = ('email', 'username')
+    list_filter = ('is_active', 'is_staff')
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
