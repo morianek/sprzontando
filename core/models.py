3869984afe53ef13e6af4995c8382785de35caa2
marcin kossakowski <morianekxd@gmail.com>
2025-01-26 20:32:06 +0100

    edited offer model to requirements

--- core/models.py
+++ core/models.py
@@ -1,18 +1,40 @@
 from django.db import models
 from authentication.models import CustomUser
 from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 # Create your models here.
 class Offer(models.Model):
+    TYPE_CHOICES = [
+        ('auto', 'Samochód'),
+        ('house', 'Cały dom'),
+        ('vacuum', 'odkurzanie'),
+        ('sweep_mop', 'Zamiatanie i mycie podłóg'),
+        ('other', 'Inne'),
+    ]
+
+    STATUS_CHOICES = [
+        ('blocked', 'Zablokwane'),
+        ('active', 'Aktywne'),
+        ('closed', 'Zakończone'),
+    ]
+
     Title = models.CharField(max_length=128)
     Description = models.TextField(blank=True)
-    Price = models.DecimalField(max_digits=6, decimal_places=2)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
     Owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='offers')
+    TimeCreated = models.DateTimeField(auto_now_add=True)
+    Type = models.CharField(max_length=10, choices=TYPE_CHOICES, default='other')
+    ExpiryDate = models.DateTimeField()
+    Status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active')
+    Location = models.CharField(max_length=256)
 
     def clean(self):
         super().clean()
-        if self.Price <= 0:
-            raise ValidationError({'Price': 'Price must be a positive number.'})
+        if self.Price is None or self.Price <= 0:
+            raise ValidationError({'Price': 'Cena musi być większa od zera.'})
+        if self.ExpiryDate <= timezone.now():
+            raise ValidationError({'ExpiryDate': 'Data wygaśnięcia musi być w przyszłości.'})
 
     def save(self, *args, **kwargs):
         self.clean()
