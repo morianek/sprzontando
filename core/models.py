746a377ebdb3de0412e4a97a2808f16b94e4f791
marcin kossakowski <morianekxd@gmail.com>
2025-01-26 19:29:34 +0100

    added offer model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from authentication.models import CustomUser
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+class Offer(models.Model):
+    Title = models.CharField(max_length=128)
+    Description = models.TextField()
+    Price = models.DecimalField(max_digits=6, decimal_places=2)
+    Owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='offers')
+
+    def clean(self):
+        super().clean()
+        if self.Price <= 0:
+            raise ValidationError({'Price': 'Price must be a positive number.'})
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
