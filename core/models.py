65e30204aa92ae4887872ec1b3ec62668791099b
marcin kossakowski <morianekxd@gmail.com>
2025-01-26 19:35:13 +0100

    altered offer model

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ValidationError
 # Create your models here.
 class Offer(models.Model):
     Title = models.CharField(max_length=128)
-    Description = models.TextField()
+    Description = models.TextField(blank=True)
     Price = models.DecimalField(max_digits=6, decimal_places=2)
     Owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='offers')
 
