b256fefb273461bccb14376a463aae3dad50eaef
marcin kossakowski <morianekxd@gmail.com>
2025-01-26 20:52:41 +0100

    sorting by time in main list

--- core/views.py
+++ core/views.py
@@ -5,5 +5,5 @@ from .models import Offer
 # Create your views here.
 
 def main(request):
-    offers = Offer.objects.all()
+    offers = Offer.objects.all().order_by('-TimeCreated')
     return render(request, 'core/main.html', {'offers': offers})
