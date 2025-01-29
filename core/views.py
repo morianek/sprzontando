be7c3bfceb3df21bf652cf8c39b528c76bdb44b4
marcin kossakowski <morianekxd@gmail.com>
2025-01-30 00:10:24 +0100

    added filters to main

--- core/views.py
+++ core/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 
 from .models import Offer
+from .choices import STATE_CHOICES, TYPE_CHOICES
 
 # Create your views here.
 
 def main(request):
     offers = Offer.objects.all().order_by('-TimeCreated')
-    return render(request, 'core/main.html', {'offers': offers})
+
+    type_filter = request.GET.get('type')
+    price_min = request.GET.get('price_min')
+    price_max = request.GET.get('price_max')
+    location_filter = request.GET.get('location')
+
+    if type_filter:
+        offers = offers.filter(Type=type_filter)
+    if price_min:
+        offers = offers.filter(Price__gte=price_min)
+    if price_max:
+        offers = offers.filter(Price__lte=price_max)
+    if location_filter:
+        offers = offers.filter(Location=location_filter)
+
+    return render(request, 'core/main.html', {
+        'offers': offers,
+        'state_choices': STATE_CHOICES,
+        'type_choices': TYPE_CHOICES,
+    })
