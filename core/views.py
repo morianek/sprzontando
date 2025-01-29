5c41870ea0a1062db33ff82725f43631313e0928
marcin kossakowski <morianekxd@gmail.com>
2025-01-30 00:40:04 +0100

    fixed filters

--- core/views.py
+++ core/views.py
@@ -11,7 +11,7 @@ def main(request):
     type_filter = request.GET.get('type')
     price_min = request.GET.get('price_min')
     price_max = request.GET.get('price_max')
-    location_filter = request.GET.get('location')
+    state_filter = request.GET.get('state')
 
     if type_filter:
         offers = offers.filter(Type=type_filter)
@@ -19,9 +19,8 @@ def main(request):
         offers = offers.filter(Price__gte=price_min)
     if price_max:
         offers = offers.filter(Price__lte=price_max)
-    if location_filter:
-        offers = offers.filter(Location=location_filter)
-
+    if state_filter:
+        offers = offers.filter(State=state_filter)
     return render(request, 'core/main.html', {
         'offers': offers,
         'state_choices': STATE_CHOICES,
