f492e19f8495d1c05e433aeb462182ee418c9072
marcin kossakowski <morianekxd@gmail.com>
2025-01-29 16:10:34 +0100

    now change password email and username is working

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+from .models import CustomUser
+
 # Create your views here.
 def login_view(request):
     if request.user.is_authenticated:
@@ -35,6 +37,26 @@ def user_settings(request):
     if not request.user.is_authenticated:
         return redirect('login')
 
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        if username == request.user.username and email == request.user.email:
+            messages.info(request, 'Nie dokonano żadnych zmian')
+            return redirect('user_settings')
+
+        try:
+            if username:
+                request.user.username = username
+            if email:
+                request.user.email = email
+
+            request.user.save()
+            messages.success(request, 'Pomyślnie zaktualizowano dane użytkownika')
+        except Exception as e:
+            messages.error(request, f'Wystąpił błąd: {e}')
+
+        return redirect('user_settings')
+
     return render(request, 'settings/user_settings.html')
 
 def site_settings(request):
@@ -46,5 +68,31 @@ def site_settings(request):
 def change_password(request):
     if not request.user.is_authenticated:
         return redirect('login')
+
+    if request.method == 'POST':
+        old_password = request.POST.get('old_password')
+        new_password = request.POST.get('new_password1')
+        confirm_password = request.POST.get('new_password2')
+
+        if not request.user.check_password(old_password):
+            messages.error(request, 'Stare hasło jest nieprawidłowe')
+            return redirect('change_password')
+
+        if new_password != confirm_password:
+            messages.error(request, 'Nowe hasła nie są takie same')
+            return redirect('change_password')
+
+        if new_password == old_password:
+            messages.error(request, 'Nowe hasło nie może być takie samo jak stare')
+            return redirect('change_password')
+
+        try:
+            request.user.set_password(new_password)
+            request.user.save()
+            messages.success(request, 'Pomyślnie zmieniono hasło')
+        except Exception as e:
+            messages.error(request, f'Wystąpił błąd: {e}')
+
+        return redirect('user_settings')
 
     return render(request, 'settings/change_password.html')
