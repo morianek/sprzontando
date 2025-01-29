c03a67a6865dd9a2bf35c2f9038d3d9e6faad252
marcin kossakowski <morianekxd@gmail.com>
2025-01-30 00:29:01 +0100

    minor changes

--- sprzontando/settings.py
+++ sprzontando/settings.py
@@ -15,7 +15,6 @@ from config import get_config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
 
 config = get_config()
 
