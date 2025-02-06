1c6cac3db8a1149bc3fad41e79b0c8d0499b4ad9
marcin kossakowski <morianekxd@gmail.com>
2025-02-06 18:36:00 +0100

    added page to see users own offers

--- user/views.py
+++ user/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
-def test(request):
-    return render(request, 'user/test.html')
+def my_offers(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    user_offers = request.user.offers.all()
+    print(user_offers)
+    return render(request, 'user/offers.html', {'offers': user_offers})
