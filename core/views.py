02bedc2e3f7a34683cf4058e23569d37b796a203
marcin kossakowski <morianekxd@gmail.com>
2025-02-06 18:12:12 +0100

    added a page with best users

--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.db.models import Avg, Value, Count
+from django.db.models.functions import Coalesce
+
+from authentication.models import CustomUser, Review
 
 from .models import Offer
 from .choices import STATE_CHOICES, TYPE_CHOICES
@@ -25,4 +29,13 @@ def main(request):
         'offers': offers,
         'state_choices': STATE_CHOICES,
         'type_choices': TYPE_CHOICES,
+    })
+
+def ranking(request):
+    best_users = CustomUser.objects.annotate(
+        avg_rating=Coalesce(Avg('user__rating'), Value(0.0)),
+        review_count=Count('user__rating')
+    ).filter(avg_rating__gt=0).order_by('-avg_rating')[:5]
+    return render(request, 'core/ranking.html', {
+        'users': best_users,
     })
