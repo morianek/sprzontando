22c8aa9d9e9be4bba8f2b9d5543b753d677325ea
marcin kossakowski <morianekxd@gmail.com>
2025-02-06 17:32:24 +0100

    added description to user model

--- authentication/models.py
+++ authentication/models.py
@@ -28,6 +28,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(max_length=500, blank=True)
 
     objects = CustomUserManager()
 
