77a171b51407837a3d7cb9ce0f82df7d80ac62b6
marcin kossakowski <morianekxd@gmail.com>
2025-03-10 15:48:15 +0100

    added ability to apply for offers from specific offer page

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('ranking/', views.ranking, name='ranking'),
     path('offer/<int:offer_id>/', views.specific_offer, name='specific_offer'),
+    path('offer/<int:offer_id>/apply/', views.apply_for_offer, name='apply_for_offer'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
 from django.db.models import Avg, Value, Count
 from django.db.models.functions import Coalesce
+from django.contrib import messages
 
 from authentication.models import CustomUser
 
-from .models import Offer
+from .models import Offer, ApplicationForOffer
 from .choices import STATE_CHOICES, TYPE_CHOICES
 
 # Create your views here.
@@ -43,8 +45,47 @@ def ranking(request):
     })
 
 def specific_offer(request, offer_id):
-    offer = Offer.objects.get(pk=offer_id)
+    try:
+        offer = Offer.objects.get(pk=offer_id)
+
+    except Offer.DoesNotExist:
+        messages.error(request, 'Nie znaleziono oferty o podanym ID')
+        return redirect('main')
+
+    did_user_apply = False
+    if request.user.is_authenticated:
+        did_user_apply = ApplicationForOffer.objects.filter(offer=offer, user=request.user).exists()
 
     return render(request, 'core/specific_offer.html', {
         'offer': offer,
+        'did_user_apply': did_user_apply,
     })
+
+def apply_for_offer(request, offer_id):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    if request.method == 'POST':
+        offer = Offer.objects.get(pk=offer_id)
+        user = request.user
+
+        if not user or not offer:
+            messages.error(request, 'Wystąpił błąd podczas zgłaszania się do oferty')
+            return redirect(specific_offer, offer_id)
+
+        if user == offer.Owner:
+            messages.error(request, 'Nie możesz zgłosić się do swojej oferty')
+            return redirect(specific_offer, offer_id)
+
+        if ApplicationForOffer.objects.filter(offer=offer, user=user).exists():
+            messages.error(request, 'Już zgłosiłeś się do tej oferty')
+            return redirect(specific_offer, offer_id)
+
+        try:
+            ApplicationForOffer(offer=offer, user=user).save()
+            messages.success(request, 'Zgłoszenie do oferty zostało złożone')
+
+        except IntegrityError:
+            messages.error(request, 'Wystąpił błąd podczas zgłaszania się do oferty')
+
+    return redirect(specific_offer, offer_id)
